a0974d9a6408cf7c525318b588ba377c91f8b9f1
OvrK12 <krauso@tcd.ie>
2025-10-15 22:52:29 +0200

    Update. Text missing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   content: ['*.html', '_site/*.html','_site/**/*.html', '_pages/**/*.md', '_posts/**/*.md', '_layouts/**/*.html', '_includes/**/*.html'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        background: "#0f172a",
-        primaryText: "#E2E8F0",
+        background: "#faf2e9",
+        dark_background: "#131f3d",
+        primaryText: "#103434",
+        link_color: "#6b4423",
+        link: {
+          DEFAULT: "#0f766e",
+          hover: "#14b8a6",
+        },
+        //primaryText: "#2d2d2d",
         gray: colors.slate,
         primary: colors.rose,
         secondary: colors.indigo,
         tertiary: colors.slate,
         danger: colors.red,
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans]
@@ -25,6 +37,9 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio')
+    require('@tailwindcss/aspect-ratio'),
+    plugin(function ({ addVariant }) {
+      addVariant('group-[.glass-effect]', ':merge(.group).glass-effect &');
+    }),
   ]
 }
